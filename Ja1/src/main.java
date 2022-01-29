9dfafd8bbfdcbc8ff189a4fba89ee13231010bd5
Kaltblotsin <Kaltblotsin@gmail.com>
2022-01-29 19:26:34 +0300

    1

---
+++ Ja1/src/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+
+    public static void main(String[] args) {
+        String text = "Hello World!";
+        boolean hyi = true;
+
+        int number = 3;
+        System.out.println("hyi");
+    }
+}
