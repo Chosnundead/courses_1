b133fe9d7c8e42aa3f731bc0e20fd9090a66707a
Kaltblotsin <Kaltblotsin@gmail.com>
2022-05-13 12:06:08 +0300

    21

---
+++ J1/bundle.js
@@ -0,0 +1,42 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!****************!*\
+  !*** ./app.js ***!
+  \****************/
+console.log("Денис");
+console.log(5);
+console.log('Добро ' + 'пожаловать ' + ' на курс'); ///*alert(2019);*/
+
+console.log(2019 - 200);
+document.getElementById("six").innerHTML = "Hello world!";
+document.getElementById("seven").innerHTML = 12 * 12;
+document.querySelector("div.out-8").innerHTML = "task-8";
+document.querySelector("div.out-10").innerHTML = "<h2>Hi</h2>";
+document.querySelector("div.out-11").innerHTML = "123";
+document.querySelector("div.out-11").innerHTML += "456";
+var a = document.querySelector("div.out-12");
+a.innerHTML = 3.1415;
+var out13 = document.querySelector("div.out-13");
+out13.innerHTML = "<img src=\"https://itgid.info/img/js20/js20_unit_01.png\" alt=\"js\">";
+var z1 = 6,
+    z2 = 3;
+document.querySelector("div.out-14").innerHTML = z1 * z2;
+var y1 = 6,
+    y2 = 3;
+document.querySelector("div.out-15").innerHTML = y1 / y2;
+var x1 = 'Hello',
+    x2 = 5;
+document.querySelector("div.out-16").innerHTML = x1 + x2;
+var out17 = document.querySelector("div.out-17");
+console.log(out17);
+var out18 = document.querySelector("div.out-18");
+console.log(out18);
+out18 = 5;
+console.log(out18);
+var out19 = document.querySelector("div.out-19-test");
+console.log(out19);
+var out20 = document.querySelector("div.out-20");
+out20.textContent = "<h2>Hi</h2>";
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
